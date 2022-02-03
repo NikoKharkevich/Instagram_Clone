a4b2bba93651b8e9a619a53fbdc3e0b8c73632cd
Nikola Kharkevich <niko.kharkevych@gmail.com>
2022-02-03 13:44:24 +0200

    FeedCell and FeedView

---
+++ Instagram_Clone/View/Feed/FeedCell.swift
@@ -0,0 +1,83 @@
+//
+//  FeedCell.swift
+//  Instagram_Clone
+//
+//  Created by Nikola Kharkevich on 03.02.2022.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image("ted")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 36, height: 36)
+                    .clipped()
+                    .cornerRadius(18)
+                Text("tedmosby")
+                    .font(.system(size: 14, weight: .semibold))
+            }
+            .padding([.leading, .bottom], 8)
+            
+            Image("ted")
+                .resizable()
+                .scaledToFill()
+                .frame(maxHeight: 440)
+                .clipped()
+            
+            HStack(spacing: 16) {
+                Image(systemName: "heart")
+                    .resizable()
+                    .scaledToFill()
+                    .foregroundColor(.black)
+                    .frame(width: 20, height: 20)
+                    .font(.system(size: 20))
+                    .padding(4)
+                
+                Image(systemName: "bubble.right")
+                    .resizable()
+                    .scaledToFill()
+                    .foregroundColor(.black)
+                    .frame(width: 20, height: 20)
+                    .font(.system(size: 20))
+                    .padding(4)
+                
+                Image(systemName: "paperplane")
+                    .resizable()
+                    .scaledToFill()
+                    .foregroundColor(.black)
+                    .frame(width: 20, height: 20)
+                    .font(.system(size: 20))
+                    .padding(4)
+            }
+            .padding(.leading, 4)
+            .foregroundColor(.black)
+            
+            Text("25 likes")
+                .font(.system(size: 14, weight: .semibold))
+                .padding(.leading, 8)
+                .padding(.bottom, 0.5)
+            
+            HStack {
+                Text("tedmosby").font(.system(size: 14, weight: .semibold)) + Text("I look great")
+                    .font(.system(size: 14))
+            }
+            .padding(.horizontal, 8)
+            
+            Text("2H")
+                .font(.system(size: 14))
+                .foregroundColor(.gray)
+                .padding(.leading, 8)
+                .padding(.top, -2)
+        }
+    }
+}
+
+struct FeedCell_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedCell()
+    }
+}
--- Instagram_Clone/View/Main/MainView.swift
+++ Instagram_Clone/View/Main/MainView.swift
@@ -12,14 +12,29 @@ struct MainView: View {
     var body: some View {
         TabView {
             FeedView()
+                .tabItem {
+                    Image(systemName: "house")
+                }
             
             SearchView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
             
             UploadPostView()
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
             
             NotificationsView()
+                .tabItem {
+                    Image(systemName: "heart")
+                }
             
             ProfileView()
+                .tabItem {
+                    Image(systemName: "person")
+                }
         }
     }
 }
