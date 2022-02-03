e1dcd37cf62027f421e76abd77de3f8639b6c557
Nikola Kharkevich <niko.kharkevych@gmail.com>
2022-02-03 20:18:27 +0200

    setup UI in RegisterView and amend CustomTextField

--- Instagram_Clone/Utils/CustomSecureField.swift
+++ Instagram_Clone/Utils/CustomSecureField.swift
@@ -11,26 +11,37 @@ struct CustomSecureField: View {
     
     @Binding var text: String
     let placeholder: Text
-    let imageName: String
     
     var body: some View {
         ZStack(alignment: .leading) {
             if text.isEmpty {
                 placeholder
-                    .foregroundColor(Color(.init(white: 1, alpha: 0.8)))
+                    .foregroundColor(.black)
                     .padding(.leading, 40)
             }
             
             HStack {
-                Image(systemName: imageName)
+                Image(systemName: "lock")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 20, height: 20)
-                    .foregroundColor(.white)
+                    .foregroundColor(.black)
+                    .padding(.trailing, 8)
                 
                 SecureField("", text: $text)
-            }
+            }.padding(.horizontal)
         }
+        .frame(width: 360, height: 50) // important
+        .background(
+            ZStack {
+                Color(.init(white: 0.9, alpha: 0.7))
+                    .cornerRadius(8)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color(.init(white: 0.7, alpha: 0.7)), lineWidth: 1)
+                    )
+            }
+            
+        )
     }
 }
-
--- Instagram_Clone/View/Authentification/RegisterView.swift
+++ Instagram_Clone/View/Authentification/RegisterView.swift
@@ -14,25 +14,61 @@ struct RegisterView: View {
     
     
     var body: some View {
-        VStack {
-            Image("instagram-text-logo")
-                .renderingMode(.template) // allows to change a color of the image
-                .resizable()
-                .scaledToFit()
-                .frame(width: 220, height: 220)
-                .foregroundColor(.black)
+        
+        NavigationView {
             
-            CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
-                .padding()
-                .cornerRadius(10)
-                .foregroundColor(.gray)
-                .padding(.horizontal, 32)
-            
-            CustomSecureField(text: $password, placeholder: Text("Email"), imageName: "envelope")
-                .padding()
-                .cornerRadius(10)
-                .foregroundColor(.gray)
-                .padding(.horizontal, 32)
+            VStack {
+                Image("instagram-text-logo")
+                    .renderingMode(.template) // allows to change a color of the image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 220, height: 220)
+                    .foregroundColor(.black)
+                
+                VStack(spacing: -16) {
+                    CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
+                        .padding()
+                        .padding(.horizontal, 32)
+                    
+                    CustomSecureField(text: $password, placeholder: Text("Password"))
+                        .padding()
+                        .padding(.horizontal, 32)
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Text("Forgot pasword?")
+                            .font(.system(size: 13, weight: .semibold))
+                            .foregroundColor(.gray)
+                            .padding(.top)
+                            .padding(.trailing, 28)
+                    }
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Register")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .clipShape(Capsule())
+                        .padding()
+                }
+                
+                Spacer()
+                
+                NavigationLink(destination: SigninView(), label: {
+                    HStack {
+                        Text("Don't have any account?")
+                            .font(.system(size: 14, weight: .semibold))
+                        
+                        Text("Sign In")
+                            .font(.system(size: 14))
+                    }
+                })
+            }
         }
     }
 }
